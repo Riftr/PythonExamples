5bc528c4229a4d2313a69ec65ecec576529c9963
Rob <nospam@nospamme.com>
2020-01-30 03:16:28 +1000

    Updated comments.py again

---
+++ Comments.py
@@ -0,0 +1,31 @@
+# This is a single line comment
+
+# This is a block comment. It's the same as a single line comment.
+#
+# Tips:
+#
+# 1. Limit the line length to 72 characters
+# 2. Use proper sentences 
+# 3. Start with a capital letter
+
+
+print("This is not a comment!")   # This is an inline comment
+
+
+
+"""This is a docuementation string
+
+Tips:
+
+1. When these appear on the first line of a funciton, class, method
+   or module they are used to document that code.
+2. Try to comment all non-trivial blocks of code 
+3. Put the last three quotation marks on their own line
+
+"""
+
+
+"""However, if you only use one line, keep it on one line"""
+
+
+# For more information see https://www.python.org/dev/peps/pep-0008/
